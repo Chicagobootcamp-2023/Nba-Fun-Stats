132f5b0cab7f028696e295d48f808ddbc597222b
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-03-13 22:20:34 -0500

    gifs are fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,13 +62,6 @@ function showTeams(list) {
   teamBox.innerHTML = listData;
 }
 
-// Clear Button
-var clearBtn = document.querySelector('.clear')
-
-function clearScreen() {
-  location.reload();
-};
-
 function displayGiphys(event) {
   event.preventDefault();
   
@@ -83,18 +76,50 @@ function displayGiphys(event) {
   .then(function(response) {
     console.log('The status of this page is', response.status + '.');
     return response.json();
+    
   }).then(function(giphs) {
     console.log('You searched for:', giphs.data);
+    imageContainer.innerHTML = ''
+
     for (var i = 0; i < giphs.data.length; i++) {
       var title = giphs.data[i].title;
       var imageTag = document.createElement('img');
       var imageTitle = document.createElement('p');
-      imageTag.setAttribute('src', giphs.data[i].images.original.url);
+      imageTag.setAttribute('src', giphs.data[i].images.original_still.url);
+      imageTag.setAttribute('data-animate', giphs.data[i].images.original.url);
+      imageTag.setAttribute('data-still', giphs.data[i].images.original_still.url);
+      imageTag.setAttribute('data-state', "still")
       imageTitle.textContent = title;
       imageContainer.append(imageTag);
     }
   });
 };
+
+// Listen for any clicks within the img-container div
+imageContainer.addEventListener("click", function(event) {
+  var element = event.target;
+  console.log('target', event.target);
+  // Check if the clicked element was an image
+  if (element.matches("img")) {
+    // Get the current value of the image's data-state attribute
+    var state = element.getAttribute("data-state");
+
+    if (state === "still") {
+      // Change the data-state attribute's value
+      // There are two different ways this attribute can be set
+      element.dataset.state = "animate";
+      element.setAttribute("data-state", "animate");
+
+      // Update the image's source to the string being stored in the data-animate attribute
+      element.setAttribute("src", element.dataset.animate);
+    } else {
+      // Change the attributes back to their non-animated values
+      element.dataset.state = "still";
+      element.setAttribute("src", element.dataset.still);
+    }
+  }
+});
+
 //Youtube Api
 //fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=surfing&key=AIzaSyD51ROkLO7Tj0gCUhdXzYzD-EmqGUCV1iI')
 // Replace YOUR_API_KEY with your actual YouTube API key
@@ -136,5 +161,4 @@ function onPlayerReady(event) {
 }
 function onPlayerStateChange(event) {}
 
-icon.addEventListener('click', displayGiphys)
-clearBtn.addEventListener('click', clearScreen);
+icon.addEventListener('click', displayGiphys);
