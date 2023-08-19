a486c560fbd12473946d786d6b2661a64bab7eaf
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-08-19 01:04:32 -0500

    finished product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,25 @@
-// user types in team and selects from dropdown menu
-// team stats and a song linked to the team will appear
-// add eventListener to the form
-// capture the users search term
-// add search term to the request url
-// add api key
-// set up our fetch call
-// handle the response
-// loop over data 
-// render data to the page
-// user clicks reset button
-// song and team stats dissappear
-
 // Autocomplete widget
 const searchWrapper = document.querySelector(".search-input");
 const inputBox = searchWrapper.querySelector("input");
 const teamBox = searchWrapper.querySelector(".autocomplete");
 const searchButton = searchWrapper.querySelector(".search-button");
-var imageContainer = document.querySelector('#giphy-images')
+var imageContainer = document.querySelector("#giphy-images");
 // let linkTag = searchWrapper.querySelector("a");
-var videoContainer = document.querySelector('#youtube-player')
+var videoContainer = document.querySelector("#youtube-player");
 let webLink;
 
 // if user press any key and release
-inputBox.onkeyup = (e)=>{
+inputBox.onkeyup = (e) => {
   let userData = e.target.value; // user entered data
   let emptyArray = [];
-  if(userData){
-    emptyArray = teams.filter((data)=>{
+  if (userData) {
+    emptyArray = teams.filter((data) => {
       // filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
       return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
     });
-    emptyArray = emptyArray.map((data)=>{
+    emptyArray = emptyArray.map((data) => {
       // passing return data inside li tag
-      return data = `<li>${data}</li>`;
+      return (data = `<li>${data}</li>`);
     });
     searchWrapper.classList.add("active"); // show autocomplete box
     showTeams(emptyArray);
@@ -44,7 +31,7 @@ inputBox.onkeyup = (e)=>{
   } else {
     searchWrapper.classList.remove("active"); // hide autocomplete box
   }
-}
+};
 
 function select(element) {
   let selectData = element.textContent;
@@ -54,56 +41,68 @@ function select(element) {
 
 function showTeams(list) {
   let listData;
-  if(!list.length){
+  if (!list.length) {
     userValue = inputBox.value;
     listData = `<li>${userValue}</li>`;
   } else {
-    listData = list.join('');
+    listData = list.join("");
   }
   teamBox.innerHTML = listData;
 }
 
 function displayGiphys(event) {
   event.preventDefault();
-  
+
   var searchTerm = inputBox.value;
-  console.log('You are currently searching for', searchTerm + '.');
-  var apiKey = 'X1UC9EboOvWecSBjWd0oHOvipre8bgHX';
-  var giphyUrl = 'https://api.giphy.com/v1/gifs/search';
+  console.log("You are currently searching for", searchTerm + ".");
+  var apiKey = "X1UC9EboOvWecSBjWd0oHOvipre8bgHX";
+  var giphyUrl = "https://api.giphy.com/v1/gifs/search";
   var offset = Math.floor(Math.random() * 200); // generate a random offset between 0 and 200
-  var requestUrl = giphyUrl + '?api_key=' + apiKey + '&limit=2&rating=g&q=' + searchTerm + '&offset=' + offset;
-  
-  // if (searchTerm !== emptyArray) {
-  //   return imageContainer.innerHTML = ''
-  // }
+  var requestUrl =
+    giphyUrl +
+    "?api_key=" +
+    apiKey +
+    "&limit=2&rating=g&q=" +
+    searchTerm +
+    "&offset=" +
+    offset;
 
   fetch(requestUrl)
-  .then(function(response) {
-    console.log('The status of this page is', response.status + '.');
-    return response.json();
-    
-  }).then(function(giphs) {
-    console.log('The data from this search is:', giphs.data + '.');
-    imageContainer.innerHTML = ''
+    .then(function (response) {
+      console.log("The status of this page is", response.status + ".");
+      return response.json();
+    })
+    .then(function (giphs) {
+      console.log("The data from this search is:", giphs.data + ".");
+      imageContainer.innerHTML = "";
 
-    for (var i = 0; i < giphs.data.length; i++) {
-      var title = giphs.data[i].title;
-      var imageTag = document.createElement('img');
-      var imageTitle = document.createElement('p');
-      imageTag.setAttribute('src', giphs.data[i].images.fixed_height_still.url);
-      imageTag.setAttribute('data-animate', giphs.data[i].images.fixed_height.url);
-      imageTag.setAttribute('data-still', giphs.data[i].images.fixed_height_still.url);
-      imageTag.setAttribute('data-state', "still")
-      imageTitle.textContent = title;
-      imageContainer.append(imageTag);
-    }
-  });
-};
+      for (var i = 0; i < giphs.data.length; i++) {
+        var title = giphs.data[i].title;
+        var imageTag = document.createElement("img");
+        var imageTitle = document.createElement("p");
+        imageTag.setAttribute(
+          "src",
+          giphs.data[i].images.fixed_height_still.url
+        );
+        imageTag.setAttribute(
+          "data-animate",
+          giphs.data[i].images.fixed_height.url
+        );
+        imageTag.setAttribute(
+          "data-still",
+          giphs.data[i].images.fixed_height_still.url
+        );
+        imageTag.setAttribute("data-state", "still");
+        imageTitle.textContent = title;
+        imageContainer.append(imageTag);
+      }
+    });
+}
 
 // Listen for any clicks within the img-container div
-imageContainer.addEventListener("click", function(event) {
+imageContainer.addEventListener("click", function (event) {
   var element = event.target;
-  console.log('target', event.target);
+  console.log("target", event.target);
   // Check if the clicked element was an image
   if (element.matches("img")) {
     // Get the current value of the image's data-state attribute
@@ -124,7 +123,17 @@ imageContainer.addEventListener("click", function(event) {
     }
   }
 
-  $(".generated-content").show();
+  $(".gif-section").show();
 });
 
-searchButton.addEventListener('click', displayGiphys);
+document.addEventListener("DOMContentLoaded", function () {
+  const generatedYouTube = document.querySelector(".youtube-section");
+  const generatedGiphy = document.querySelector(".gif-section");
+
+  searchButton.addEventListener("click", function () {
+    generatedYouTube.style.display = "flex";
+    generatedGiphy.style.display = "flex";
+  });
+});
+
+searchButton.addEventListener("click", displayGiphys);
